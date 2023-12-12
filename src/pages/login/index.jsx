94499b51ee67570452eb986a930a89defe3063fe
João Rafael <95690569+jrafaelfda@users.noreply.github.com>
2023-12-11 21:15:55 -0300

    att tela de login, tela de recuperacao e home

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,50 +1,59 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import "../../assets/global.css"; 
+import "../../components/LayoutComponents/styles.css";
 
-import { LayoutComponents } from "../../components/LayoutComponents";
-
-export const Login = () => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    navigate('/home'); 
+  };
+  
   return (
-    <LayoutComponents>
-      <form className="login-form">
-        <span className="login-form-title"> Bem vindo </span>
+    <div className="container-login">
+      <div className="wrap-login">
+        <form className="login-form" onSubmit={handleLogin}> 
+          <span className="login-form-title"> Login </span>
 
-        <span className="login-form-title">
-        </span>
+          <div className="wrap-input">
+            <input
+              className={email !== "" ? "has-val input" : "input"}
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Usuario"
+            />
+          </div>
 
-        <div className="wrap-input">
-          <input
-            className={email !== "" ? "has-val input" : "input"}
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <span className="focus-input" data-placeholder="Email"></span>
-        </div>
+          <div className="wrap-input">
+            <input
+              className={password !== "" ? "has-val input" : "input"}
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Senha"
+            />
+          </div>
 
-        <div className="wrap-input">
-          <input
-            className={password !== "" ? "has-val input" : "input"}
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <span className="focus-input" data-placeholder="Password"></span>
-        </div>
+          <div className="container-login-form-btn">
+            <button type="submit" className="login-form-btn">Acessar</button> 
+          </div>
 
-        <div className="container-login-form-btn">
-          <button className="login-form-btn">Login</button>
-        </div>
-
-        <div className="text-center">
-          <span className="txt1">Não possui conta? </span>
-          <Link className="txt2" to="/register">
-            Criar conta.
-          </Link>
-        </div>
-      </form>
-    </LayoutComponents>
+          <div className="text-center">
+            <Link className="txt1" to="/recovery">
+              Esqueceu a senha?
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
+
+export default Login; 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "../pages/login";
-import { Register } from "../pages/register";
+import Login from "../pages/login";
+import Recovery from "../pages/recovery"; 
+import Home from "../pages/home"; 
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Adicione uma rota para a raiz '/' */}
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/recovery" element={<Recovery />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 };
+
+
 
